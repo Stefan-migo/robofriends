4cc139988b6b1a4ed1a8f66824cd0e214f0a7016
Stefan-migo <stefan.migo@gmail.com>
2022-07-06 16:22:03 -0400

    first commit

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+
+const Card = ({ name, email, id }) => {
+    return ( 
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow shadow-5'>
+            <img src={`https://robohash.org/${id}?200x200`} alt='robot' />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import 'tachyons';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
